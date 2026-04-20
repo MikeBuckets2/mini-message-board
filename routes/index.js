d8f62ed962de25bd62c60af2dc7e07ca63f1e47b
MikeBuckets2 <michaelmorris121@yahoo.com>
2026-04-20 14:28:01 +0000

    feat: add messages array to index router

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,26 @@ const { Router } = require("express");
 
 const router = Router();
 
+const messages = [
+  {
+    id: 1,
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    id: 2,
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 router.get("/", (req, res) => {
-  res.send("Index route working");
+  res.render("index", {
+    title: "Mini Messageboard",
+    messages: messages,
+  });
 });
 
 module.exports = router;
